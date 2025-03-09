006c906dca520b1362c59e6459292be075ef464e
daijunfei <djfmail@163.com>
2025-03-09 08:47:33 +0800

    初始代码

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import TonConnect from '@tonconnect/sdk';
+import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('Wallet demo')
+
+  return (
+    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+      <div className="app-container">
+        <h1>{message}</h1>
+        <div className="content">
+          <TonConnectButton />
+          <button onClick={() => setMessage('你点击了按钮！')}>
+            点击我
+          </button>
+          <p>这是一个使用Vite和React创建的简单前端应用</p>
+        </div>
+      </div>
+    </TonConnectUIProvider>
+  )
+}
+
+export default App
